4da0833619658d3bc29667b898509f1efa6cdec0
jkjoycn <131505779+jkjoycn@users.noreply.github.com>
2024-11-07 09:21:47 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,6 +39,7 @@ if (memoDom) {
         getNextList();
     });
 }
+
 window.onload = function() {
     let offset = 0;
     const limit = 10;
@@ -52,6 +53,7 @@ window.onload = function() {
             memoname = userData.nickname;
             userurl = `${memo.host}/u/${userData.username}`;
             description = userData.description;
+            memo.username = userData.username; // 确保 memo 对象中有 username 属性
 
             // 更新 banner 信息
             const bannerSubinfo = document.getElementById('info');
@@ -63,7 +65,8 @@ window.onload = function() {
        .catch(error => {
             console.error('Error fetching user data:', error);
         });
-    
+}
+
 function getFirstList() {
     fetch(`${memoUrl}&pageToken=${nextPageToken}`).then(res => res.json()).then(resdata => {
         updateHTML(resdata.memos);
@@ -146,7 +149,6 @@ function updateHTML(data) {
         }
 
         const relativeTime = getRelativeTime(new Date(item.createTime));
-        //const avatarurl = `${memo.host}file/users/${memo.creatorId}/avatar`;
 
         memoResult += `<li class="timeline"><div class="memos__content" style="--avatar-url: url(${avatarurl})"><div class="memos__text"><div class="memos__userinfo"><div>${memoname}</div><div><svg viewBox="0 0 24 24" aria-label="认证账号" class="memos__verify"><g><path d="M22.5 12.5c0-1.58-.875-2.95-2.148-3.6.154-.435.238-.905.238-1.4 0-2.21-1.71-3.998-3.818-3.998-.47 0-.92.084-1.336.25C14.818 2.415 13.51 1.5 12 1.5s-2.816.917-3.437 2.25c-.415-.165-.866-.25-1.336-.25-2.11 0-3.818 1.79-3.818 4 0 .494.083.964.237 1.4-1.272.65-2.147 2.018-2.147 3.6 0 1.495.782 2.798 1.942 3.486-.02.17-.032.34-.032.514 0 2.21 1.708 4 3.818 4 .47 0 .92-.086 1.335-.25.62 1.334 1.926 2.25 3.437 2.25 1.512 0 2.818-.916 3.437-2.25.415.163.865.248 1.336.248 2.11 0 3.818-1.79 3.818-4 0-.174-.012-.344-.033-.513 1.158-.687 1.943-1.99 1.943-3.484zm-6.616-3.334l-4.334 6.5c-.145.217-.382.334-.625.334-.143 0-.288-.04-.416-.126l-.115-.094-2.415-2.415c-.293-.293-.293-.768 0-1.06s.768-.294 1.06 0l1.77 1.767 3.825-5.74c.23-.345.696-.436 1.04-.207.346.23.44.696.21 1.04z"></path></g></svg></div><div class="memos__id">@${memo.username}</div></div><p>${memoContREG}</p></div><div class="memos__meta"><small class="memos__date">${relativeTime} • From「<a href="${memo.host}m/${item.id}" target="_blank">Memos</a>」</small></div></div></li>`;
     }
@@ -195,97 +197,108 @@ if (localTheme) {
     document.body.classList.add(localTheme);
 }
 
-themeToggle.addEventListener("click", () => {
-    const themeUndefined = !new RegExp("(dark|light)-theme").test(document.body.className);
-    const isOSDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    themeToggle.addEventListener("click", () => {
+        const themeUndefined = !new RegExp("(dark|light)-theme").test(document.body.className);
+        const isOSDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-    if (themeUndefined) {
-        if (isOSDark) {
-            document.body.classList.add("light-theme");
-        } else {
-            document.body.classList.add("dark-theme");
-        }
-    } else {
-        document.body.classList.toggle("light-theme");
-        document.body.classList.toggle("dark-theme");
-    }
-
-    window.localStorage &&
-        window.localStorage.setItem(
-            "theme",
-            document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme",
-        );
-    // Images lightbox
-window.ViewImage && ViewImage.init('.container img');
-});
-// Darkmode End
-
-// 解析豆瓣 Start
-function fetchDB() {
-    var dbAPI = 'https://api.loliko.cn';
-    var dbA = document.querySelectorAll(".timeline a[href*='douban.com/subject/']:not([rel='noreferrer'])") || '';
-    if (dbA) {
-        for (var i = 0; i < dbA.length; i++) {
-            _this = dbA[i]
-            var dbHref = _this.href
-            var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
-            var db_type = dbHref.replace(db_reg, "$1");
-            var db_id = dbHref.replace(db_reg, "$2").toString();
-            if (db_type == 'movie') {
-                var this_item = 'movie' + db_id;
-                var url = dbAPI + "movies/" + db_id;
-                if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-                    fetch(url).then(res => res.json()).then(data => {
-                        let fetch_item = 'movies' + data.sid;
-                        let fetch_href = "https://movie.douban.com/subject/" + data.sid + "/"
-                        localStorage.setItem(fetch_item, JSON.stringify(data));
-                        movieShow(fetch_href, fetch_item)
-                    });
-                } else {
-                    movieShow(dbHref, this_item)
-                }
-            } else if (db_type == 'book') {
-                var this_item = 'book' + db_id;
-                var url = dbAPI + "v2/book/id/" + db_id;
-                if (localStorage.getItem(this_item) == null || localStorage.getItem(this_item) == 'undefined') {
-                    fetch(url).then(res => res.json()).then(data => {
-                        let fetch_item = 'book' + data.id;
-                        let fetch_href = "https://book.douban.com/subject/" + data.id + "/"
-                        localStorage.setItem(fetch_item, JSON.stringify(data));
-                        bookShow(fetch_href, fetch_item)
-                    });
-                } else {
-                    bookShow(dbHref, this_item)
-                }
+        if (themeUndefined) {
+            if (isOSDark) {
+                document.body.classList.add("light-theme");
+            } else {
+                document.body.classList.add("dark-theme");
             }
-        }// for end
+        } else {
+            document.body.classList.toggle("light-theme");
+            document.body.classList.toggle("dark-theme");
+        }
+
+        window.localStorage &&
+            window.localStorage.setItem(
+                "theme",
+                document.body.classList.contains("dark-theme") ? "dark-theme" : "light-theme",
+            );
+    });
+    // Darkmode End
+
+    // 解析豆瓣 Start
+    function fetchDB() {
+        var dbAPI = 'https://api.loliko.cn';
+        var dbA = document.querySelectorAll(".timeline a[href*='douban.com/subject/']:not([rel='noreferrer'])") || '';
+        if (dbA) {
+            const promises = [];
+            for (var i = 0; i < dbA.length; i++) {
+                _this = dbA[i];
+                var dbHref = _this.href;
+                var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
+                var db_type = dbHref.replace(db_reg, "$1");
+                var db_id = dbHref.replace(db_reg, "$2").toString();
+                if (db_type == 'movie') {
+                    var this_item = 'movie' + db_id;
+                    var url = dbAPI + "movies/" + db_id;
+                    if (localStorage.getItem(this_item) == null) {
+                        promises.push(fetch(url).then(res => res.json()).then(data => {
+                            let fetch_item = 'movies' + data.sid;
+                            let fetch_href = "https://movie.douban.com/subject/" + data.sid + "/";
+                            localStorage.setItem(fetch_item, JSON.stringify(data));
+                            movieShow(fetch_href, fetch_item);
+                        }).catch(error => {
+                            console.error('Error fetching movie data:', error);
+                        }));
+                    } else {
+                        movieShow(dbHref, this_item);
+                    }
+                } else if (db_type == 'book') {
+                    var this_item = 'book' + db_id;
+                    var url = dbAPI + "v2/book/id/" + db_id;
+                    if (localStorage.getItem(this_item) == null) {
+                        promises.push(fetch(url).then(res => res.json()).then(data => {
+                            let fetch_item = 'book' + data.id;
+                            let fetch_href = "https://book.douban.com/subject/" + data.id + "/";
+                            localStorage.setItem(fetch_item, JSON.stringify(data));
+                            bookShow(fetch_href, fetch_item);
+                        }).catch(error => {
+                            console.error('Error fetching book data:', error);
+                        }));
+                    } else {
+                        bookShow(dbHref, this_item);
+                    }
+                }
+            }// for end
+            Promise.all(promises).then(() => {
+                console.log('All fetch operations completed');
+            });
+        }
     }
-}
 
-function movieShow(fetch_href, fetch_item) {
-    var storage = localStorage.getItem(fetch_item);
-    var data = JSON.parse(storage);
-    var db_star = Math.ceil(data.rating);
-    var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.name + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.director + " / 类型：" + data.genre + " / " + data.year + "</time><section class='post-preview--excerpt'>" + data.intro.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.img + "></div>"
-    var db_div = document.createElement("div");
-    var qs_href = ".timeline a[href='" + fetch_href + "']"
-    var qs_dom = document.querySelector(qs_href)
-    qs_dom.parentNode.replaceChild(db_div, qs_dom);
-    db_div.innerHTML = db_html
-}
+    function movieShow(fetch_href, fetch_item) {
+        var storage = localStorage.getItem(fetch_item);
+        var data = JSON.parse(storage);
+        var db_star = Math.ceil(data.rating);
+        var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.name + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.director + " / 类型：" + data.genre + " / " + data.year + "</time><section class='post-preview--excerpt'>" + data.intro.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.img + "></div>";
+        var db_div = document.createElement("div");
+        var qs_href = ".timeline a[href='" + fetch_href + "']";
+        var qs_dom = document.querySelector(qs_href);
+        if (qs_dom) {
+            qs_dom.parentNode.replaceChild(db_div, qs_dom);
+            db_div.innerHTML = db_html;
+        }
+    }
 
-function bookShow(fetch_href, fetch_item) {
-    var storage = localStorage.getItem(fetch_item);
-    var data = JSON.parse(storage);
-    var db_star = Math.ceil(data.rating.average);
-    var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating.average + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section class='post-preview--excerpt'>" + data.summary.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.images.medium + "></div>"
-    var db_div = document.createElement("div");
-    var qs_href = ".timeline a[href='" + fetch_href + "']"
-    var qs_dom = document.querySelector(qs_href)
-    qs_dom.parentNode.replaceChild(db_div, qs_dom);
-    db_div.innerHTML = db_html
-}
-// 解析豆瓣 End
+    function bookShow(fetch_href, fetch_item) {
+        var storage = localStorage.getItem(fetch_item);
+        var data = JSON.parse(storage);
+        var db_star = Math.ceil(data.rating.average);
+        var db_html = "<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' rel='noreferrer' href='" + fetch_href + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating.average + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section class='post-preview--excerpt'>" + data.summary.replace(/\s*/g, "") + "</section></div></div><img referrer-policy='no-referrer' loading='lazy' class='post-preview--image' src=" + data.images.medium + "></div>";
+        var db_div = document.createElement("div");
+        var qs_href = ".timeline a[href='" + fetch_href + "']";
+        var qs_dom = document.querySelector(qs_href);
+        if (qs_dom) {
+            qs_dom.parentNode.replaceChild(db_div, qs_dom);
+            db_div.innerHTML = db_html;
+        }
+    }
+    // 解析豆瓣 End
 
-
-
+    // Images lightbox
+    window.ViewImage && ViewImage.init('.container img');
+});
